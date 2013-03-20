ba1badb663fa7a7328c9ed009835981e2026cbe4
sakuramilk <c.sakuramilk@gmail.com>
2013-03-20 13:26:52 +0900

    felica: add jb-mr1 path

--- sbin/felica_init.sh
+++ sbin/felica_init.sh
@@ -1,9 +1,15 @@
 #!/system/bin/sh
 
-FELICA_KEY_FILE=/data/media/TweakS3A/felica_key
+FELICA_KEY_FILE_JB=/data/media/TweakS3A/felica_key
+if [ -f $FELICA_KEY_FILE_JB ]; then
+    FELICA_KEY=`cat $FELICA_KEY_FILE_JB`
+    BASE_CMDLINE=`cat /proc/cmdline`
+    echo "cordon=$FELICA_KEY $BASE_CMDLINE" > /proc/cmdline
+fi
 
-if [ -f $FELICA_KEY_FILE ]; then
-    FELICA_KEY=`cat $FELICA_KEY_FILE`
+FELICA_KEY_FILE_JB_MR1=/data/media/0/TweakS3A/felica_key
+if [ -f $FELICA_KEY_FILE_JB_MR1 ]; then
+    FELICA_KEY=`cat $FELICA_KEY_FILE_JB_MR1`
     BASE_CMDLINE=`cat /proc/cmdline`
     echo "cordon=$FELICA_KEY $BASE_CMDLINE" > /proc/cmdline
 fi

2bb1f005e39eb59499c68ee5bc02d5fb73a9e9d8
ma34s <ma34s.sss@gmail.com>
2013-04-12 21:59:10 +0900

    [felica]support eXtreme Tuner

--- sbin/felica_init.sh
+++ sbin/felica_init.sh
@@ -1,5 +1,19 @@
 #!/system/bin/sh
 
+CMDLINE_FILE_JB=/data/media/kbc/GNT2eXtremeTuner/cmdline
+if [ -f $CMDLINE_FILE_JB ]; then
+    FELICA_CMDLINE=`cat $CMDLINE_FILE_JB`
+    echo "$FELICA_CMDLINE" > /proc/cmdline
+    exit 0
+fi
+
+CMDLINE_FILE_JB_MR1=/data/media/0/kbc/GNT2eXtremeTuner/cmdline
+if [ -f $CMDLINE_FILE_JB_MR1 ]; then
+    FELICA_CMDLINE=`cat $CMDLINE_FILE_JB_MR1`
+    echo "$FELICA_CMDLINE" > /proc/cmdline
+    exit 0
+fi
+
 FELICA_KEY_FILE_JB=/data/media/TweakS3A/felica_key
 if [ -f $FELICA_KEY_FILE_JB ]; then
     FELICA_KEY=`cat $FELICA_KEY_FILE_JB`

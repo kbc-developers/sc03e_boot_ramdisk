e9982647822222c48aff8aa0bcb2903f5764fcc1
ma34s <ma34s.sss@gmail.com>
2013-05-19 00:25:53 +0900

    support custom ro.sf.lcd_density

--- sbin/init.prop.sh
+++ sbin/init.prop.sh
@@ -1,13 +1,25 @@
 #!/system/bin/sh
 
 PROP_PATH=/data/TweakGS3.prop
+KBC_PROP_PATH=/data/kbc_preferred.prop
+
+if [ -f $KBC_PROP_PATH ] ; then
+	PROP_PATH=$KBC_PROP_PATH
+fi
+
+LCD_DENSITY=`/sbin/getprefprop $PROP_PATH ro.sf.lcd_density`
 
 setprop ro.kbc.propwritable 1
 
-LCD_DENSITY=`grep ro\.sf\.lcd_density $PROP_PATH | cut -d'=' -f2`
+setprop ro.kbc.preferred.prop 1
+
 if [ -n "$LCD_DENSITY" ]; then
-    setprop ro.sf.lcd_density $LCD_DENSITY
+
+#    setprop ro.sf.lcd_density $LCD_DENSITY
+	setprop qemu.sf.lcd_density $LCD_DENSITY
+	
 fi
 
 setprop ro.kbc.propwritable 0
+
 
